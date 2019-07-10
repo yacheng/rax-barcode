d61e603218ff1f754555fc58f7ef24924cd34114
浴风 <liangzhi.llz@alibaba-inc.com>
2019-07-10 17:47:03 +0800

    小程序版本加入

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   'extends': ['eslint-config-rax/typescript'],
   'globals': {
     "Component": true,
-    "Page": true
+    "Page": true,
+    "my":true
   },
   'rules': {
     "no-return-assign": "off",
+    "@typescript-eslint/no-use-before-define":"off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-explicit-any": "off",
